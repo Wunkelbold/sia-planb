efa1a6bdc9fc10c02c4adec26add7449b764b770
MOPSKATER <wolberjo@hs-albsig.de>
2024-11-08 13:20:46 +0100

    add first test

--- sia-backend/test/test.sh
+++ sia-backend/test/test.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-echo "All tests OK"
-exit 0
+echo "Starting Tests"
+
+echo "#1 Check index.html availability"
+statuscode="$(curl -s -o /dev/null -w "%{http_code}" http://flask)"
+echo Response: $statuscode
+if test statuscode = "200"; then
+    failed="$failed #1"
+fi
+
+if test -z "$failed"; then
+    echo "All tests OK"
+    exit 0
+fi
+
+echo "Failed at tests $failed"
+exit 1

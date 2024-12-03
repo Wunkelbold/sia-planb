25e9fbcd9cbcab2dfc81921aac3204ed706c3c34
MOPSKATER <wolberjo@hs-albsig.de>
2024-12-03 11:46:40 +0000

    move event routes to eventhandler

---
+++ sia-backend/flask-server/Sections/EventHandler.py
@@ -0,0 +1,24 @@
+from globals import app
+from flask import render_template, jsonify
+from permissions import require_permissions
+from database import *
+
+def getAllEvents() -> list[Tables.Event]:
+    Tables.Event.query.all()
+
+def getActiveEvents() -> list[Tables.Event]:
+    return Tables.Event.query.filter_by(visibility=True).all()
+
+@app.route("/events",methods=['GET'])
+def events():
+    return render_template('events.html', title='Sia-PlanB.de', events=getActiveEvents())
+
+@app.route("/eventmanager",methods=['GET'])
+@require_permissions("eventmanager.show")
+def eventmanager():
+    return render_template('eventmanager.html', title='Sia-PlanB.de', events=getAllEvents())
+
+@app.route("/api/events/all",methods=['GET'])
+@require_permissions("events.all")
+def eventmanager():
+    return jsonify(getAllEvents())

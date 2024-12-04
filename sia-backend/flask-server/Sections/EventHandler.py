c94bec2f80f85c092e99781f5e11a9d203af4c12
MOPSKATER <wolberjo@hs-albsig.de>
2024-12-04 11:37:17 +0000

    add event rout and fix other routes

--- sia-backend/flask-server/Sections/EventHandler.py
+++ sia-backend/flask-server/Sections/EventHandler.py
@@ -1,24 +1,29 @@
 from globals import app
 from flask import render_template, jsonify
-from permissions import require_permissions
+from permissions import require_permissions, hasPermissions
 from database import *
 
 def getAllEvents() -> list[Tables.Event]:
-    Tables.Event.query.all()
+    return Tables.Event.query.all()
 
-def getActiveEvents() -> list[Tables.Event]:
-    return Tables.Event.query.filter_by(visibility=True).all()
-
-@app.route("/events",methods=['GET'])
+@app.route("/events", methods=['GET'])
 def events():
-    return render_template('events.html', title='Sia-PlanB.de', events=getActiveEvents())
+    return render_template('events.html', title='Sia-PlanB.de', events=getAllEvents(), hasPermissions=hasPermissions)
 
-@app.route("/eventmanager",methods=['GET'])
+@app.route("/eventmanager", methods=['GET'])
 @require_permissions("eventmanager.show")
 def eventmanager():
     return render_template('eventmanager.html', title='Sia-PlanB.de', events=getAllEvents())
 
-@app.route("/api/events/all",methods=['GET'])
-@require_permissions("events.all")
+@app.route("/api/events/all", methods=['GET'])
+@require_permissions("events.getall")
 def apiGetAllEvents():
     return jsonify(getAllEvents())
+
+@app.route("/api/events/get/<int:eventid>", methods=['GET'])
+def apiGetEvent(eventid: int):
+    if hasPermissions(f"/api/events/get/{eventid}"):
+        event = Tables.Event.query.filter_by(id=eventid).first_or_404()
+        return jsonify(event, mimetype='application/json')
+    else:
+        return app.login_manager.unauthorized()

d230a70e54874bd9b9047c411aa31a443c9c8c43
Wunkebold <->
2024-12-18 10:12:52 +0100

    sada

--- sia-backend/flask-server/app.py
+++ sia-backend/flask-server/app.py
@@ -169,6 +169,7 @@ def contact():
         )
         db.session.add(newContact)
         db.session.commit()
+        flash("Danke für deine Nachricht!")
     if current_user.is_authenticated:
         if current_user.email: form.email.data=current_user.email 
         if current_user.surname: form.surname.data=current_user.surname 
